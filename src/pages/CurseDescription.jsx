3cf408740dc4dd5e1348974ee8130bea9e5e4d57
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2024-08-23 19:55:24 -0300

    Remove unnecessary Tailwind classes from CurseDescription component

--- src/pages/CurseDescription.jsx
+++ src/pages/CurseDescription.jsx
@@ -1,21 +1,17 @@
 const CurseDescription = () => {
   return (
-    <div className='w-screen h-screen bg-white'>
-
-      <main className='w-full p-10 flex flex-col justify-center md:flex-row'>
-        <div className='w-full md:w-2/5 md:h-60 mx-auto bg-gray-200'>
+    <div>
+      <main className='w-full p-10 flex flex-col justify-center'>
+        <div className='w-full mx-auto'>
           <img className='object-cover' src='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' alt='Imagen sin fondo' />
         </div>
 
-        <div className='md:w-3/5 md:px-10'>
-          <h2 className='font-bold text-lg sm:text-xl text-gray-900 text-start mt-8 md:mt-0'>NOMBRE DEL PROYECTO</h2>
-          <h3 className='font-bold text-lg sm:text-xl text-gray-900 text-start mt-6'>ORGANIZADOR</h3>
-          <h3 className='font-bold text-lg sm:text-xl text-gray-900 text-start mt-6'>DURACIÓN</h3>
-          <p className='font-normal text-sm sm:text-lg text-gray-900 text-start mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla soluta iste repellat eum sequi officia expedita unde aperiam voluptatum! Quis assumenda cupiditate voluptatem, excepturi voluptas accusamus atque perferendis architecto. Dolor? Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium commodi illum quasi voluptas numquam, cum, adipisci reprehenderit neque enim eveniet fugit, molestias blanditiis laudantium iusto aliquam optio ut aperiam!</p>
+        <h2 className='font-bold text-lg text-start mt-8'>NOMBRE DEL PROYECTO</h2>
+        <h3 className='font-bold text-lg text-start mt-6'>ORGANIZADOR</h3>
+        <h3 className='font-bold text-lg text-start mt-6'>DURACIÓN</h3>
+        <p className='font-normal text-sm text-start mt-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla soluta iste repellat eum sequi officia expedita unde aperiam voluptatum! Quis assumenda cupiditate voluptatem, excepturi voluptas accusamus atque perferendis architecto. Dolor? Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium commodi illum quasi voluptas numquam, cum, adipisci reprehenderit neque enim eveniet fugit, molestias blanditiis laudantium iusto aliquam optio ut aperiam!</p>
 
-          <button className='rounded-none border-gray-600 text-lg	bg-gray-300 text-gray-600 mt-8 py-3 hover:bg-gray-200 hover:border-gray-200 focus:outline-none'>Inscribirme ahora</button>
-        </div>
-
+        <button className='rounded-none border-gray-600 text-lg	bg-gray-300 mt-8 py-3 focus:outline-none'>Inscribirme ahora</button>
       </main>
     </div>
   );
