23669d74f740b2fe460c2c0a46a72c1f70b1379c
andrea <matopayes04@gmail.com>
2024-09-13 12:03:30 -0300

    .

---
+++ src/components/SubsInputField.jsx
@@ -0,0 +1,49 @@
+
+import { useMask } from '@react-input/mask';
+
+const stylesFormInput = 'flex items-center rounded-2xl bg-gray-300 p-4';
+const stylesFormLabel = 'text-sm font-semibold text-gray-600 mb-1';
+const stylesFromContainer = 'flex flex-col mb-4';
+
+export const SubsInputField = (props) => {
+  const { className, inputPorps, textLabel, placeholder, type, id, typeMask } =
+    props;
+
+  const getTypeMask = () => {
+    switch (typeMask || '') {
+      case 'phone':
+        return {
+          mask: '+54 (___) ___-_____',
+          replacement: { _: /\d/ },
+        };
+      case 'dni':
+        return {
+          mask: '__.___.___',
+          replacement: { _: /\d/ },
+        };
+      default:
+        return {
+          mask: '____________________________',
+          replacement: { _: /\D/ },
+        };
+    }
+  };
+
+  const inputRef = useMask(getTypeMask());
+
+  return (
+    <div className={`${stylesFromContainer} ${className}`}>
+      <label className={`${stylesFormLabel}`} htmlFor={id}>
+        {textLabel}
+      </label>
+      <input
+        {...inputPorps}
+        ref={inputRef}
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        className={`${stylesFormInput}`}
+      />
+    </div>
+  );
+};
