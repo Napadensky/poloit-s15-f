5653f2b7bdf5b9a9bca0cc422484710234e44cd4
andrea <matopayes04@gmail.com>
2024-09-17 20:28:42 -0300

    feat/estilos para botones

--- src/pages/subscriptions/SubsForm.jsx
+++ src/pages/subscriptions/SubsForm.jsx
@@ -70,10 +70,6 @@ const SubsForm = () => {
           options={['Talento tech', 'Silver tech', 'Forge']}
         />
 
-        
-           
-          
-       
         <SubsInputField
           placeholder={'Ingrese link de constancia'}
           textLabel={'Link de constancia'}
@@ -81,17 +77,17 @@ const SubsForm = () => {
           id={'link'}
         />
 
-        <div className='flex justify-between'>
+        <div className='flex justify-between gap-4'>
           <button
             type='button'
-            className='focus:shadow-outline text-blue hover:bg-[#71AJD6]focus:outline-none rounded-xl border border-blue-500 px-4 py-2 focus:bg-[#F4F5F6]'
+            className='focus:shadow-outline text-blue hover:bg-[#71AJD6]focus:outline-none w-full rounded-xl border border-blue-500 px-4 py-2 focus:bg-[#F4F5F6]'
           >
             Cancelar
           </button>
 
           <button
             type='submit'
-            className='focus:shadow-outline hover:bg-[#71AJD6]focus:outline-none rounded-xl bg-[#2F68A1] px-4 py-2 font-bold text-white focus:bg-[#2F68A1]'
+            className='focus:shadow-outline hover:bg-[#71AJD6]focus:outline-none w-full rounded-xl bg-[#2F68A1] px-4 py-2 font-bold text-white focus:bg-[#2F68A1]'
           >
             Inscribirme
           </button>
