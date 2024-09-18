f1b5fbf70bca74b2ef48011ccf0b882888b546e8
andrea <matopayes04@gmail.com>
2024-09-18 20:03:36 -0300

    feat/refine login

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,9 +1,47 @@
-const LoginForm = () => {
-    return(
-        <div>
-            <h1>Formulario Login</h1>
-        </div>
-    )
-}
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/polo-it 2.svg';
+import { SubsInputField } from '@/components/SubsInputField';
 
-export {LoginForm}
+export const LoginForm = () => {
+  const navigate = useNavigate()
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center rounded-3xl bg-gray-100'>
+      <div className='mb-4 flex justify-center'>
+        <img src={logo} alt='Logo PoloIT' className='hover:brightness-200' />
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log('submit');
+          console.log({ e });
+          console.log(e.target.username.value);
+          console.log(e.target.password.value);
+          navigate('/dashboard')
+
+
+        }}
+      >
+        <SubsInputField
+          placeholder={'usuario'}
+          textLabel={'Usuario'}
+          type={'text'}
+          id={'username'}
+        />
+
+        <SubsInputField
+          placeholder={'**********'}
+          textLabel={'Contraseña'}
+          type={'password'}
+          id={'password'}
+        />
+
+        <button
+          type='submit'
+          className='focus:shadow-outline hover:bg-[#71AJD6]focus:outline-none w-full rounded-xl bg-[#2F68A1] px-4 py-2 font-bold text-white focus:bg-[#2F68A1]'
+        >
+          Iniciar sesion
+        </button>
+      </form>
+    </div>
+  );
+};
