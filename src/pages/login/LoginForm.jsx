eeb42f36a3d17ad2c33856122aa441748cd29676
andrea <matopayes04@gmail.com>
2024-09-20 16:56:40 -0300

    feat/agregue y refine estilos del login

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -3,9 +3,9 @@ import logo from '../../assets/polo-it 2.svg';
 import { SubsInputField } from '@/components/SubsInputField';
 
 export const LoginForm = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center rounded-3xl bg-gray-100'>
+    <div className='min-sceen flex w-full flex-col items-center justify-center rounded-xl bg-gray-100 px-4 md:m-auto md:max-w-96 md:border-2 md:border-blue-400 md:p-20'>
       <div className='mb-4 flex justify-center'>
         <img src={logo} alt='Logo PoloIT' className='hover:brightness-200' />
       </div>
@@ -16,9 +16,7 @@ export const LoginForm = () => {
           console.log({ e });
           console.log(e.target.username.value);
           console.log(e.target.password.value);
-          navigate('/dashboard')
-
-
+          navigate('/dashboard');
         }}
       >
         <SubsInputField
@@ -27,14 +25,12 @@ export const LoginForm = () => {
           type={'text'}
           id={'username'}
         />
-
         <SubsInputField
           placeholder={'**********'}
           textLabel={'Contraseña'}
           type={'password'}
           id={'password'}
         />
-
         <button
           type='submit'
           className='focus:shadow-outline hover:bg-[#71AJD6]focus:outline-none w-full rounded-xl bg-[#2F68A1] px-4 py-2 font-bold text-white focus:bg-[#2F68A1]'
