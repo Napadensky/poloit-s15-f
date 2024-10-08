c3ce4406a8c8e59cad0e665614caa9f41f19b5e5
andrea <matopayes04@gmail.com>
2024-10-07 22:02:42 -0300

    formateo de documento

--- src/hooks/useSquads.jsx
+++ src/hooks/useSquads.jsx
@@ -1,34 +1,28 @@
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-export const useSquads =(url)=> {
-    const [squad, setSquad]= useState(null);
-    const [loading, setLoading]= useState(true);
-    const [error, setError] = useState (null);
+export const useSquads = (url) => {
+  const [squad, setSquad] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async ()=> {
-            try {
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error('repuesta incorrecta');
-                   
-                }
-   
-      const data = await response.json();
-      setSquad(data);
-      
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-    
-  };
-  fetchData();
-},[url]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('repuesta incorrecta');
+        }
+
+        const data = await response.json();
+        setSquad(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
 
   return { squad, loading, error };
-                
-            
-        
 };
