88074652409c4d04f52c40c08a4af9b55068cc1f
Georgina Zamboni <Geor2015zamboni@gmail.com>
2024-10-11 23:41:32 -0300

    feat/fixing-pages-and-icons

--- src/pages/dashboard/DashHome.jsx
+++ src/pages/dashboard/DashHome.jsx
@@ -2,6 +2,11 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getProjects, deleteProject } from '@/services/projectServices';
 import { SubsInputCard } from '@/components/SubsInputCard';
+import {
+  MagnifyingGlassIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
 
 const DashHome = () => {
   console.log(import.meta.env);
@@ -29,9 +34,6 @@ const DashHome = () => {
   };
   return (
     <div className='my-4 flex w-full flex-col items-center gap-4'>
-      <h1 className='text-lg font-semibold text-[#262A2C] lg:mr-auto'>
-        Proyectos
-      </h1>
       {projects.map((project) => (
         <div
           key={project._id}
@@ -47,20 +49,7 @@ const DashHome = () => {
               className='flex flex-row items-center'
               to={`/dashboard/project/${project._id}`}
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='#60B635'
-                className='size-4 lg:size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-                />
-              </svg>
+              <MagnifyingGlassIcon className='size-4 stroke-[#60B635] lg:size-6' />
 
               <button className='mr-6 bg-none text-sm font-medium text-[#60B635] underline lg:text-lg'>
                 Ver más
@@ -70,39 +59,13 @@ const DashHome = () => {
               className='flex flex-row items-center'
               to={`/dashboard/project/edit/${project._id}`}
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='#2563eb'
-                className='size-4 lg:size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
-                />
-              </svg>
+              <PencilSquareIcon className='size-4 stroke-[#2563eb] lg:size-6' />
               <button className='mr-6 bg-none text-sm font-medium text-blue-600 underline lg:text-lg'>
                 Editar
               </button>
             </Link>
             <Link className='flex flex-row items-center' to={`/dashboard/`}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='#dc2626'
-                className='size-4 lg:size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
-                />
-              </svg>
+              <TrashIcon className='size-4 stroke-[#dc2626] lg:size-6' />
               <button
                 className='bg-none text-sm font-medium text-red-600 underline lg:text-lg'
                 onClick={() => handleDelete(project._id)}
--- src/pages/dashboard/dashProject/DashProjectEdit.jsx
+++ src/pages/dashboard/dashProject/DashProjectEdit.jsx
@@ -7,6 +7,8 @@ import {
 import { useNavigate, useParams } from 'react-router-dom';
 import { DashInputCheckbox } from '@/components/DashInputCheckbox';
 import { DashInputField } from '@/components/DashInputField';
+import { ArrowUpTrayIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 const DashProjectEdit = () => {
   const { projectId } = useParams();
@@ -18,10 +20,8 @@ const DashProjectEdit = () => {
   const [file, setFile] = useState(null);
   const [preview, setPreview] = useState(null);
   const navigate = useNavigate();
+  const [originalProject, setOriginalProject] = useState(null);
 
-  useEffect(() => {
-    console.log('Estado actual del proyecto:', project);
-  }, [project]);
   useEffect(() => {
     const fetchProjectAndTags = async () => {
       try {
@@ -44,6 +44,7 @@ const DashProjectEdit = () => {
           projectData.img = `${import.meta.env.VITE_API_URL}${projectData.img}`;
         }
         setProject(projectData);
+        setOriginalProject(projectData);
         setTags(tagsData);
         setPreview(projectData.img);
       } catch (error) {
@@ -131,8 +132,6 @@ const DashProjectEdit = () => {
           .split('T')[0];
       }
 
-      console.log('Proyecto editado (con fechas ajustadas):', updatedProject);
-
       setProject(updatedProject);
       setModal(true);
       setConfirmation(false);
@@ -159,9 +158,9 @@ const DashProjectEdit = () => {
     }
   };
   const handleReset = () => {
-    setProject(null);
+    setProject(originalProject);
     setFile(null);
-    setPreview(null);
+    setPreview(originalProject.img);
   };
 
   if (isLoading) {
@@ -177,29 +176,29 @@ const DashProjectEdit = () => {
       <form
         onSubmit={handleSubmit}
         encType='multipart/form-data'
-        className='grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8'
+        className='grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-4'
       >
         <h1 className='text-xl font-bold text-neutral-800 lg:hidden'>
           Crear proyecto
         </h1>
-        <h2 className='mb-4 hidden content-center text-xl font-medium text-[#2F68A1] lg:order-2 lg:block'>
+        <h2 className='hidden content-center text-xl font-medium text-[#2F68A1] lg:order-2 lg:block'>
           Sobre el Proyecto
         </h2>
-        <div className='lg:order-3 lg:mt-auto'>
+        <div className='lg:order-3'>
           <DashInputField
             id='title'
             type='text'
             name='title'
             value={project.title}
             onChange={handleChange}
-            labelClassName='my-2 lg:my-4 text-sm lg:text-lg font-medium'
+            labelClassName=' text-sm lg:text-lg font-medium'
             textLabelMobile='¿Cuál es el título del Proyecto?'
             textLabelDesktop='Título del Proyecto'
-            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
+            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-3'
             placeholder='Ingresar título del proyecto'
           />
         </div>
-        <div className='flex flex-col lg:order-4 lg:justify-center'>
+        <div className='flex flex-col lg:order-5 lg:row-span-2 lg:justify-center'>
           <label
             htmlFor='description'
             className='my-2 text-sm font-medium lg:text-lg'
@@ -209,30 +208,30 @@ const DashProjectEdit = () => {
           <textarea
             id='description'
             name='description'
-            className='my-2 h-40 w-full resize-none overflow-auto focus:outline-none break-words rounded-xl border-0 bg-[#E7F0F8] px-4 py-6 text-sm font-medium'
+            className='my-2 h-full w-full resize-none overflow-auto break-words rounded-xl border-0 bg-[#E7F0F8] p-2 text-sm font-medium focus:outline-none'
             placeholder='Objetivos del proyecto y beneficios de la participación'
             value={project.description}
             onChange={handleChange}
           />
         </div>
-        <div className='lg:order-10'>
+        <div className='lg:order-8'>
           <DashInputField
             id='maxStudents'
             type='number'
             name='maxStudents'
-            labelClassName='my-2 text-sm lg:text-lg lg:my-6 font-medium'
-            textLabelMobile='¿Cantidad máxima de inscriptos?'
-            textLabelDesktop='Cantidad máxima de inscriptos'
-            inputClassName='my-2  lg:my-4 mt-1 w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
+            labelClassName='my-2 text-sm lg:text-lg  font-medium'
+            textLabelMobile='¿Cantidad máxima de Squads?'
+            textLabelDesktop='Cantidad máxima de Squads'
+            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-3'
             placeholder='Ingrese sólo números'
             value={project.maxStudents}
             onChange={handleChange}
           />
         </div>
-        <div className='lg:order-5'>
+        <div className='lg:order-4'>
           <DashInputCheckbox
-            className='my-4 flex flex-col justify-between gap-2'
-            labelClassName='my-4 lg:text-lg text-sm font-medium'
+            className='flex flex-col justify-between gap-2 p-3'
+            labelClassName='lg:text-lg text-sm font-medium'
             textLegend='¿Qué Roles requiere el proyecto?'
             options={tags.map((tag) => ({ value: tag._id, text: tag.name }))}
             name='tag'
@@ -240,95 +239,49 @@ const DashProjectEdit = () => {
             handleSelect={handleSelectTag}
           />
         </div>
-
         <div className='lg:order-6'>
-          <DashInputField
-            id='modalidad'
-            type='text'
-            name='modalidad'
-            value={project.modalidad}
-            onChange={handleChange}
-            labelClassName='my-2 lg:my-4 text-sm lg:text-lg font-medium'
-            textLabelMobile='¿Cuál es la modalidad del Proyecto?'
-            textLabelDesktop='Modalidad del Proyecto'
-            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
-            placeholder='Ingresar modalidad del proyecto'
-          />
-        </div>
-        <div className='lg:order-8'>
-          <DashInputField
-            id='plataforma'
-            type='text'
-            name='plataforma'
-            value={project.plataforma}
-            onChange={handleChange}
-            labelClassName='my-2 lg:my-4 text-sm lg:text-lg font-medium'
-            textLabelMobile='¿Cuál es la plataforma utilizada en el Proyecto?'
-            textLabelDesktop='Plataforma utilizada en el Proyecto'
-            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
-            placeholder='Ingresar plataforma'
-          />
-        </div>
-        <div className='lg:order-7'>
           <DashInputField
             id='startDate'
             type='date'
             name='startDate'
-            labelClassName='my-2 lg:my-4 text-sm lg:text-lg font-medium'
+            labelClassName='my-2  text-sm lg:text-lg font-medium'
             textLabelMobile='¿Cuál es la fecha de inicio del Proyecto?'
             textLabelDesktop='Fecha de inicio del Proyecto'
-            inputClassName='my-2  mt-1 w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
+            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-3'
             placeholder='yyyy/mm/dd'
             onChange={handleChange}
             value={project.startDate || ''}
           />
         </div>
-        <div className='lg:order-9'>
+        <div className='lg:order-7'>
           <DashInputField
             id='endDate'
             type='date'
             name='endDate'
-            labelClassName='my-2 lg:my-4 text-sm lg:text-lg font-medium'
+            labelClassName='my-2  text-sm lg:text-lg font-medium'
             textLabelMobile='¿Cuál es la fecha de finalización del Proyecto?'
             textLabelDesktop='Fecha de finalización del Proyecto'
-            inputClassName='my-2  mt-1 w-full rounded-xl border-0 bg-[#E7F0F8] p-6'
+            inputClassName='my-2  w-full rounded-xl border-0 bg-[#E7F0F8] p-3'
             placeholder='yyyy/mm/dd'
             onChange={handleChange}
             value={project.endDate || ''}
           />
         </div>
 
-        <div className='lg:order-1 lg:row-span-2'>
+        <div className='lg:order-1 lg:row-span-3'>
           <h2 className='my-2 text-sm font-medium lg:hidden'>
-            Subir imagen del proyecto
+            Imagen del proyecto (*.png, *.jpg, *.jepg)
           </h2>
-          <div className='h-56 w-full rounded-xl border border-dashed border-gray-300 bg-gray-100'>
-            <label
-              htmlFor='img'
-              className='flex h-full w-full items-center justify-center'
-            >
+          <div className='h-64 rounded-xl border border-dashed border-gray-300 bg-gray-100'>
+            <label htmlFor='img' className='flex h-full w-full justify-center'>
               {preview ? (
                 <img
                   src={preview}
                   alt='Imagen del proyecto'
-                  className='h-full w-full object-cover'
+                  className='h-48 w-96 object-scale-down'
                 />
               ) : (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                >
-                  <path
-                    d='M3 16.5V18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75V16.5M7.5 7.5L12 3M12 3L16.5 7.5M12 3V16.5'
-                    stroke='#D3D7D9'
-                    strokeWidth='1.5'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  />
-                </svg>
+                <ArrowUpTrayIcon className='size-6 self-center stroke-[#D3D7D9]' />
               )}
             </label>
             <input
@@ -341,18 +294,18 @@ const DashProjectEdit = () => {
             />
           </div>
         </div>
-        <div className='my-4 flex w-full flex-row justify-between lg:order-11 lg:ml-auto lg:mt-11 lg:h-fit lg:w-[28rem]'>
+        <div className='flex w-full flex-row justify-between lg:order-9 lg:ml-auto lg:mt-auto lg:h-fit lg:w-[28rem]'>
           <button
             type='button'
             onClick={handleReset}
-            className='w-40 cursor-pointer rounded-xl border-2 border-[#DD5A6B] bg-zinc-50 px-4 py-4 text-base font-semibold text-[#DD5A6B] lg:w-52 lg:border-[#2F68A1] lg:px-6 lg:text-[#2F68A1]'
+            className='w-40 cursor-pointer rounded-xl border-2 border-[#DD5A6B] bg-zinc-50 px-4 py-3 text-base font-semibold text-[#DD5A6B] lg:w-52 lg:border-[#2F68A1] lg:px-6 lg:text-[#2F68A1]'
           >
             Cancelar
           </button>
           <input
             type='submit'
             value='Editar proyecto'
-            className='cursor-pointer rounded-xl bg-[#2F68A1] px-6 py-4 text-base font-semibold text-zinc-50 lg:w-52'
+            className='cursor-pointer rounded-xl bg-[#2F68A1] px-6 py-3 text-base font-semibold text-zinc-50 lg:w-52'
           />
         </div>
       </form>
@@ -385,18 +338,7 @@ const DashProjectEdit = () => {
       {modal && (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
           <div className='flex flex-col items-center justify-center gap-3 rounded-xl border-2 border-[#60B635] bg-[#F0F9EB] p-8'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='#60B635'
-              className='size-6'
-            >
-              <path
-                fillRule='evenodd'
-                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
-                clipRule='evenodd'
-              />
-            </svg>
+            <CheckCircleIcon className='size-6 fill-[#60B635]' />
             <h2 className='mb-4 text-base font-semibold text-[#262A2C]'>
               ¡Proyecto editado correctamente!
             </h2>
